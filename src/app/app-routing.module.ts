4c53f5711298bd99224f11421745fa38969f1524
Plamen M <pulsedloops@gmail.com>
2024-02-02 11:41:34 +0100

    [feat]: Implement Create Product & Edit Product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import {ProductCardComponent} from "./product-card/product-card.component";
 
 const routes: Routes = [
-  { path: ':productId', component: ProductEditComponent },
+  { path: '', component: ProductCardComponent },
+  { path: 'products/:productId', component: ProductEditComponent },
 ];
 
 @NgModule({
